827aa7eca0b9b7d72fed92eba55a54051d8b879e
Chen Weiguang <chen.weiguang@gmail.com>
2017-12-29 11:11:11 +0800

    Made fluentd logging optional once again.

--- main.go
+++ main.go
@@ -7,19 +7,18 @@ import (
 	"os"
 	"path"
 	"regexp"
+	"time"
 
 	"github.com/BurntSushi/toml"
 	"github.com/colinmarc/hdfs"
 	"github.com/fluent/fluent-logger-golang/fluent"
+	"github.com/sirupsen/logrus"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
 	conf = kingpin.Flag("conf", "TOML config file path.").Required().ExistingFile()
 )
-
-var log *fluent.Fluent
-var tag string
 
 // fluentd Fluentd configuration
 type fluentd struct {
@@ -47,19 +46,73 @@ type config struct {
 	// Regex filters accepting the source files to copy from.
 	Filters []string
 
+	// Flag to indicate to use Fluentd logging
+	UseFluentd bool
+
 	// Fluentd configurations
 	Fluentd fluentd
 }
 
+func levelToStr(level logrus.Level) string {
+	switch level {
+	case logrus.DebugLevel:
+		return "debug"
+	case logrus.InfoLevel:
+		return "info"
+	case logrus.WarnLevel:
+		return "warning"
+	case logrus.ErrorLevel:
+		return "error"
+	case logrus.FatalLevel:
+		return "fatal"
+	case logrus.PanicLevel:
+		return "panic"
+	}
+
+	return "unknown"
+}
+
+func regularLog(level logrus.Level, heading string, msg string) {
+	logrus.WithFields(logrus.Fields{
+		"level":    levelToStr(level),
+		"heading":  heading,
+		"msg":      msg,
+		"datetime": time.Now(),
+	}).Print()
+}
+
+func genFluentdLog(logger *fluent.Fluent, tag string) func(logrus.Level, string, string) {
+	return func(level logrus.Level, heading string, msg string) {
+		logger.Post(tag, map[string]string{
+			"level":    levelToStr(level),
+			"heading":  heading,
+			"msg":      msg,
+			"datetime": time.Now().Format(time.RFC3339),
+		})
+	}
+}
+
+func genFluentdLogClose(logger *fluent.Fluent) func() {
+	return func() {
+		logger.Close()
+	}
+}
+
+var log = regularLog
+var logClose = func() {}
+
 // Function literal type to take a HDFS src path, local dst path, and HDFS client
 type pathAct func(string, string, *hdfs.Client, os.FileInfo)
 
-func walkDir(dirname string, src string, dst string, client *hdfs.Client, act pathAct) {
+func walkDir(dirname string, src string, dst string, client *hdfs.Client, act pathAct) error {
 	srcDirPath := path.Join(src, dirname)
 	dstDirPath := path.Join(dst, dirname)
 
 	fileInfo, err := client.ReadDir(srcDirPath)
-	exitOnErr("HDFS ReadDir", err)
+
+	if err != nil {
+		return err
+	}
 
 	for _, f := range fileInfo {
 		srcPath := path.Join(srcDirPath, f.Name())
@@ -71,6 +124,8 @@ func walkDir(dirname string, src string, dst string, client *hdfs.Client, act pa
 			walkDir(f.Name(), srcDirPath, dstDirPath, client, act)
 		}
 	}
+
+	return nil
 }
 
 func isMatchingFilters(srcPath string, filters []*regexp.Regexp) bool {
@@ -101,13 +156,7 @@ func isSimilarFile(srcPath string, dstPath string, client *hdfs.Client) (bool, e
 }
 
 func exitOnErrMsg(heading string, errMsg string) {
-	if log != nil {
-		log.Post(tag, map[string]string{
-			"heading": heading,
-			"error":   errMsg,
-		})
-	}
-
+	log(logrus.ErrorLevel, heading, errMsg)
 	os.Exit(1)
 }
 
@@ -118,23 +167,26 @@ func exitOnErr(heading string, err error) {
 }
 
 func initLog(c config) error {
-	l, err := fluent.New(fluent.Config{
-		FluentHost: c.Fluentd.Host,
-		FluentPort: c.Fluentd.Port,
-	})
+	if c.UseFluentd {
+		logger, err := fluent.New(fluent.Config{
+			FluentHost: c.Fluentd.Host,
+			FluentPort: c.Fluentd.Port,
+		})
 
-	if err != nil {
-		return err
+		if err != nil {
+			return err
+		}
+
+		log = genFluentdLog(logger, c.Fluentd.Tag)
+		logClose = genFluentdLogClose(logger)
 	}
 
-	log = l
-	tag = c.Fluentd.Tag
-	log.Post(tag, "hdfs-to-local log started")
-
+	log(logrus.InfoLevel, "hdfs-to-local INIT", "Log started")
 	return nil
 }
 
 func main() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
 	kingpin.Parse()
 
 	var c config
@@ -143,7 +195,7 @@ func main() {
 
 	err = initLog(c)
 	exitOnErr("initLog", err)
-	defer log.Close()
+	defer logClose()
 
 	filters := make([]*regexp.Regexp, len(c.Filters))
 
@@ -154,38 +206,46 @@ func main() {
 	}
 
 	client, err := hdfs.New(c.Host)
-	exitOnErr("HDFS New", err)
+	exitOnErr("HDFS", err)
 
 	srcStat, err := client.Stat(c.Src)
-	exitOnErr("HDFS Stat", err)
+	exitOnErr("HDFS", err)
 
 	if !srcStat.IsDir() {
-		exitOnErrMsg("HDFS src", fmt.Sprintf("Given source path '%s' is not a directory!", c.Src))
+		exitOnErrMsg("HDFS", fmt.Sprintf("Given source path %s is not a directory!", c.Src))
 	}
 
 	// recursive portion
-	walkDir("", c.Src, c.Dst, client, func(srcPath string, dstPath string, client *hdfs.Client, f os.FileInfo) {
+	err = walkDir("", c.Src, c.Dst, client, func(srcPath string, dstPath string, client *hdfs.Client, f os.FileInfo) {
 		if !f.IsDir() && isMatchingFilters(srcPath, filters) {
-			err := os.MkdirAll(path.Dir(dstPath), 0755)
-			exitOnErr("os.MkdirAll", err)
+			dstDirPath := path.Dir(dstPath)
+			err := os.MkdirAll(dstDirPath, 0755)
+
+			if err != nil {
+				log(logrus.ErrorLevel, "MKDIR", fmt.Sprintf("Error creating %s", dstDirPath))
+				return
+			}
 
 			isSimilar, err := isSimilarFile(srcPath, dstPath, client)
-			exitOnErr("isSimilarFile", err)
+
+			if err != nil {
+				log(logrus.ErrorLevel, "SIMILAR", fmt.Sprintf("Unable to compare similarity between %s and %s", srcPath, dstPath))
+				return
+			}
 
 			if isSimilar {
-				log.Post(tag, map[string]string{
-					"action": "SIMILAR",
-					"msg":    fmt.Sprintf("%s AND %s, not copying...", srcPath, dstPath),
-				})
+				log(logrus.InfoLevel, "SIMILAR", fmt.Sprintf("%s AND %s, not copying...", srcPath, dstPath))
 			} else {
-				log.Post(tag, map[string]string{
-					"action": "COPY",
-					"msg":    fmt.Sprintf("%s -> %s", srcPath, dstPath),
-				})
+				log(logrus.InfoLevel, "COPY", fmt.Sprintf("%s -> %s", srcPath, dstPath))
 
 				err = client.CopyToLocal(srcPath, dstPath)
-				exitOnErr("hdfs.Client.CopyToLocal", err)
+
+				if err != nil {
+					log(logrus.ErrorLevel, "COPY", fmt.Sprintf("%s -> %s", srcPath, dstPath))
+				}
 			}
 		}
 	})
+
+	exitOnErr("HDFS", err)
 }
